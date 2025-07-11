dd9f089f8f1f983e3153d36d9660210d9b6e4ddc
AItext1 <151544633+AItext1@users.noreply.github.com>
2025-07-10 21:37:41 -0400

    Create akile.sh

---
+++ xb/akile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "请选择要下载的 DNS 文件:"
+echo "1) hk"
+echo "2) tw"
+echo "3) sgp"
+
+read -p "输入选择 (1 或 2): " choice
+
+case $choice in
+  1) # hk_basic
+    url="https://raw.githubusercontent.com/AItext1/sh/main/hk.yml"
+    ;;
+  2) # tw_basic
+    url="https://raw.githubusercontent.com/AItext1/sh/main/tw.yml"
+    ;;
+  3) # sgp_basic
+    url="https://raw.githubusercontent.com/AItext1/sh/main/sgp.yml"
+    ;;
+  4) # jp_basic
+    url="https://raw.githubusercontent.com/AItext1/sh/main/lite_jp.yml"
+    ;;
+  5) # us_basic
+    url="https://raw.githubusercontent.com/AItext1/sh/main/lite_us.yml"
+    ;;  
+  6) # us-c_basic
+    url="https://raw.githubusercontent.com/AItext1/sh/main/lite_us-c.yml"
+    ;;
+  
+  *)
+    echo "无效选择。退出。"
+    exit 1
+    ;;
+esac
+
+wget -O /etc/soga/dns.yml "$url" && soga restart
+
+echo "DNS 更新完成并尝试重启 soga。"
+
+# 显示soga日志
+soga log
