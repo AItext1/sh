4764df39501b53e04e3594415650f037c9510ebf
AItext1 <151544633+AItext1@users.noreply.github.com>
2026-08-31 10:32:41 -0400

    Update nncdns.sh

--- nnc/nncdns.sh
+++ nnc/nncdns.sh
@@ -13,18 +13,6 @@ case $choice in
   1) # hk
     url="https://raw.githubusercontent.com/AItext1/sh/refs/heads/main/nnc/hk.yml"
     ;;
-  2) # tw
-    url="https://raw.githubusercontent.com/AItext1/sh/refs/heads/main/nnc/tw.yml"
-    ;;
-  3) # sgp
-    url="https://raw.githubusercontent.com/AItext1/sh/refs/heads/main/nnc/sgp.yml"
-    ;;
-  4) # jp
-    url="https://raw.githubusercontent.com/AItext1/sh/refs/heads/main/nnc/jp.yml"
-    ;;  
-  5) # us
-    url="https://raw.githubusercontent.com/AItext1/sh/refs/heads/main/nnc/us.yml"
-    ;;
   *)
     echo "无效选择。退出。"
     exit 1
