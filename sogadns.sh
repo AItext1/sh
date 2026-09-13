4ef383da58c20852c32202bae5428a30641ff20f
AItext1 <151544633+AItext1@users.noreply.github.com>
2026-09-13 16:28:54 -0400

    Update sogadns.sh

--- sogadns.sh
+++ sogadns.sh
@@ -2,15 +2,6 @@
 
 echo "请选择要下载的 DNS 文件:"
 echo "1) HK"
-echo "2) TW"
-echo "3) SIN"
-echo "4) JP"
-echo "5) KR"
-echo "6) US"
-echo "7) UK"
-echo "8) FR"
-echo "9) GEM"
-echo "10) CA"
 
 read -p "输入选择 (1 或 2): " choice
 
@@ -18,33 +9,6 @@ case $choice in
   1) # HK
     url="https://raw.githubusercontent.com/AItext1/sh/main/hk_basic.yml"
     ;;
-  2) # TW
-    url="https://raw.githubusercontent.com/AItext1/sh/main/tw_basic.yml"
-    ;;
-  3) # SIN
-    url="https://raw.githubusercontent.com/AItext1/sh/main/sgp_basic.yml"
-    ;;
-  4) # JP
-    url="https://raw.githubusercontent.com/AItext1/sh/main/jp_basic.yml"
-    ;;
-  5) # KR
-    url="https://raw.githubusercontent.com/AItext1/sh/main/kr_basic.yml"
-    ;;
-  6) # US
-    url="https://raw.githubusercontent.com/AItext1/sh/main/us_basic.yml"
-    ;;
-  7) # UK
-    url="https://raw.githubusercontent.com/AItext1/sh/main/uk_basic.yml"
-    ;;
-  8) # FR
-    url="https://raw.githubusercontent.com/AItext1/sh/main/fr_basic.yml"
-    ;;
-  9) # GEM
-    url="https://raw.githubusercontent.com/AItext1/sh/main/de_basic.yml"
-    ;;
-  10) # CA
-    url="https://raw.githubusercontent.com/AItext1/sh/main/ca_basic.yml"
-    ;;  
   *)
     echo "无效选择。退出。"
     exit 1
